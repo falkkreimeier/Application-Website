e710814112ba53314961054ac392ca5f4ecfde7d
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-31 15:00:14 +0100

    rework application website

--- src/App.js
+++ src/App.js
@@ -1,67 +1,46 @@
+import Kritzelkopf from "./Components/Kritzelkopf";
 import styled from "styled-components";
-import Header from "./Components/Header";
-import Profile from "./Components/Profile";
-import Welcome from "./Components/Welcome";
-import Interests from "./Components/Interests";
-import Skills from "./Components/Skills";
-import Facts from "./Components/Facts";
-import Portfolio from "./Components/Portfolio";
-import Xing from "./Components/Xing";
-import Github from "./Components/Github";
-import LinkToLakritzelHomepage from "./Components/LinkToLakritzelHomepage";
-import BlogText from "./Components/BlogText";
-import Footer from "./Components/Footer";
 
 function App() {
   return (
     <Container>
-      <Header />
-      <Profile />
-      <Welcome />
-      <Interests />
-      <Skills />
-      <Facts />
-      <Portfolio />
-      <Xing />
-      <Github />
-      <LinkToLakritzelHomepage />
-      <BlogText />
-      <Footer />
+      <Kritzelkopf />
     </Container>
   );
 }
 
 const Container = styled.div`
   margin: 0 auto;
-  width: 100%;
-  height: 100%;
+  height: 100vh;
+  max-width: 1000px;
   display: grid;
   grid-template-areas:
-    "head head head head"
-    "profileArea welcome welcome welcome"
-    "profileArea interests skills portfolio"
-    "facts xing github lakritzelHomepage"
-    "facts blogText blogText blogText"
+    "header header header header"
+    "profile welcome welcome welcome"
+    "profile interests skills portfolio"
+    "facts xing github linkedin"
+    "facts techstack cv cv"
     "footer footer footer footer";
   grid-gap: 0.2rem;
   text-align: center;
   font-family: "PT Serif", serif;
 
-  @media only screen and (max-width: 850px) {
+  @media only screen and (max-width: 800px) {
     width: 375px;
     grid-gap: 5px;
     grid-template-areas:
-      "head"
-      "profileArea"
+      "header"
+      "profile"
       "welcome"
       "interests"
+      "facts"
       "skills"
       "portfolio"
-      "facts"
+      "cv"
+      "techstack"
       "xing"
       "github"
-      "lakritzelHomepage"
-      "blogText"
+      "linkedin"
       "footer";
   }
 `;
