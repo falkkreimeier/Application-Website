0fd07306f9b149b9da7bcda299ad7808d90ae7d2
Falk Kreimeier <f.kreimeier@gmx.net>
2021-11-03 19:32:38 +0100

    add media query

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,6 +14,9 @@ const Wrapper = styled.header`
   color: black;
   border-radius: 5px;
   padding: var(--main-padding);
+  @media only screen and (max-width: 375px) {
+    max-width: 410;
+  }
 `;
 
 const Headline = styled.h1`
